dc12294b1a4df31708e838ceb1ad3bb83480c63f
jasonli5 <jasonli2068@gmail.com>
2024-12-03 17:48:41 -0500

    fetch call

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-function a() {
-  alert("Hello world");
+function onSubmit(e) {
+  e.preventDefault();
+
+  const fetchArguments = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  fetch("https://pokeapi.co/api/v2/pokemon/ditto", fetchArguments)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      apiData = data;
+      console.log(apiData.name);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
